7d6f1536eac29c6b32eae759723db98e6559f3d3
Carlos Cobaleda <96302091+ccobaledaTL@users.noreply.github.com>
2026-07-23 10:32:18 -0500

    Update tripleliftBidAdapter.js

--- modules/tripleliftBidAdapter.js
+++ modules/tripleliftBidAdapter.js
@@ -160,11 +160,6 @@ function _buildPostBody(bidRequests, bidderRequest) {
       }
     }
 
-    if (bidRequest.params.sharedPublisherId) {
-      imp.ext = imp.ext || {};
-      imp.ext.sharedPublisherId = bidRequest.params.sharedPublisherId;
-    }
-
     return imp;
   });
 
